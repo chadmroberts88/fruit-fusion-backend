f7f896ae5054b7b1d9f6b1fd7164781c38868457
chadmroberts88 <chadmroberts88@gmail.com>
2022-08-26 09:50:33 -0230

    First test passed.

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
 const handler = require('./handler');
 
-test('Should return 200.', async () => {
-
-  // need some mock event
-  const sampleEvent = {
-    pathParameters: {
-      id: "1"
-    }
-  };
-
-  const response = await handler.getUser(sampleEvent);
-
-  expect(response.statusCode).toEqual(200);
+describe('GetUser', () => {
+  test('Should return 200.', async () => {
+    const sampleEvent = {
+      pathParameters: {
+        id: "1"
+      }
+    };
+    const response = await handler.getUser(sampleEvent);
+    expect(response.statusCode).toEqual(200);
+  });
 });
